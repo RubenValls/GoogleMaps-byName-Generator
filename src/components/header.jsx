3aeb61a93b28c6bed04b8c438698186829671200
Rubén Valls <r.vallsaparici@gmail.com>
2023-01-12 14:01:00 +0100

    Header added

---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div id="headerimg" class="jumbotron bg-cover text-white">
+            <div class="container-fluid py-5 text-center">
+                <h1 class="display-3 font-weight-bold">Google Maps Finder</h1>
+                <h5 class="font-italic mb-0">Write a city and change Google Map location.</h5>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
